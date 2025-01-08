4a3219363fc1535f3f29fcc92b72c947143f5bfd
Akshitha-eng <thathireddyakshithareddy@gmail.com>
2025-01-08 10:27:53 +0530

    pushing the files to repo

---
+++ looping.js
@@ -0,0 +1,20 @@
+//! for loop
+//for(let i=1;i<=5;i++){
+   // console.log("Good Morning");
+//}
+
+let n = 100;
+for(let i = 0; i<=n;i++)
+{
+    if(i%2==0){
+        console.log(i + "is a even number");
+
+    }else{
+        console.log(i + " is a odd number");
+
+    }
+    
+}
+
+
+
