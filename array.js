06f491c1bbf0cfaebdc934b1c5507a417f31f997
Akshitha-eng <thathireddyakshithareddy@gmail.com>
2025-01-20 20:07:01 +0530

    demo3

--- array.js
+++ array.js
@@ -1,44 +1,115 @@
-/*const user1 = "sita";
-const user2 = "gita";
-const user3 = "ram";
-const user4 = "priya";
-const user5 = "sam";
-console.log(user1);
-console.log(user2);
-console.log(user3);
-console.log(user4);
-console.log(user5);
+let user1 = "Ramya";
+ let user2 = "SaiSmitha";
+ let user3 = "Sai nikhil";
+ let user4 = "Rahul";
+ let user5 = "Prabhas";
 
-const users = ["sita" ,"gita" ,"priya" ,"sidhu" ,"ram"];
-console.log(users[0]);
-console.log(users[3]);
-for(let i=0;i< users.length;i++){
-    console.log(users);
-}*/
+ console.log(user1);
+ console.log(user2);
+ console.log(user3);
+ console.log(user4);
+ console.log(user5);
 
-/*const users = ["sita" ,"gita" ,"priya" ,"sidhu" ,"ram"];
-users.push("Arun");
-console.log(users);
+ let users = ["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+ console.log(users[0]);
+ console.log(users[4]);
+ for(let i = 0 ; i<users.length; i++)
+ {
+     console.log(users[i]);
+ }
 
-users.pop();
-console.log(users);*/
 
-/*let users = ["Ramya" , "Saismitha" , "Rahul"];
-let x = users.forEach((user))=>{
-    return user;
-});
-console.log(x);
-*/
+let users = ["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+ let x = users.map((user)=>{
+//     // console.log(user);
+     return user; //!["Ramya" , "SaiSmitha" , "Sai Nikhil" , "Rahul" , "Prabhas" , "NTR"];
+ });
+ console.log(x);
 
-let UserDetails = {
+ let x = users.forEach((user)=>{
+//     // console.log(user);
+     return user;
+ });
+ console.log(x); // !undefined
+
+
+//! create array of objects and print the values.
+
+let userDetails = {
     name : "Pranav",
-    age:22,
-    company :"Amazon",
-    sal : 1234567890,
+     age:22,
+     company :"Amazon",
+     sal : 1234567890,
     details:function(){
-        return My name is ${this.name} and i am working ${company}
-    }
+         return `My name is ${this.name} and i am working in ${this.company}`
+     }
+ };
+ console.log(userDetails);
+ console.log(userDetails.name);
+ console.log(userDetails.details());
+
+//! create nested object and access the property of nested object
+
+ let userDetails = {
+     name : "Pranav",
+     age:22,
+    company :"Amazon",
+     sal : 1234567890,
+     address:{
+         city:"Hyd",
+         area :{
+             areaName:"Hitech",
+             pincode :123456
+         }
+     }
+
+ }
+
+ console.log(userDetails.address.city);
+ console.log(userDetails.address.area.areaName);
+
+//! create array of objects and print the values.
+
+ let usersDetails = [
+     {
+         name:"Ramya",
+         city:"Guntur"
+     },
+     {
+         name:"Sai Nikhil",
+         city:"Karimnagar"
+     },
+     {
+         name:"Rahul",
+        city:"Ananthpur"
+    },
+     {
+         name:"Vaishali",
+         city:"Nizambad"
+     },
+     {
+         name:"Priya",
+         city:"Kadapa"
+     }
+ ];
+
+ usersDetails.map((x)=>{
+     console.log(x.name);
+ })
+
+ console.log(usersDetails[3].name);
+
+//! JSON 
+
+let userDetails ={
+    name:"Sam",
+    age:25,
+    city:"Hyd"
 };
 console.log(userDetails);
-console.log(userDetails.name);
-console.log(userDetails.details());
+
+let x = JSON.stringify(userDetails);
+console.log("JSON object "+x);
+
+let  y = JSON.parse(x);
+console.log(y);
