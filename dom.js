30fedb1d7ab8228d435deacef70e8c81fbc83d67
Akshitha-eng <thathireddyakshithareddy@gmail.com>
2025-01-24 20:31:03 +0530

    demo6

--- dom.js
+++ dom.js
@@ -10,3 +10,20 @@ let ele = document.getElementById("test");
 // ele.innerText= "<h1>Header</h1>"
 ele.innerHTML = "<h1>Header</h1>"
 console.log(ele);
+
+let ele = document.getElementsByClassName("test");
+console.log(ele);
+console.log(Array.isArray(ele));
+ele[0].innerText="Hello"
+
+let x = [...ele];
+//console.log(x , Array.isArray(x));
+x.map(element=>{
+    element.style.backgroundColor="orange"
+})
+
+
+let ele = document.querySelectorAll(".test");
+console.log(ele);
+
+
