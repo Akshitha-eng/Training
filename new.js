f79cba233829fd8bc83c7fd75c8b6097754be4e4
Akshitha-eng <thathireddyakshithareddy@gmail.com>
2025-01-29 22:15:33 +0530

    demo9

---
+++ new.js
@@ -0,0 +1,10 @@
+let employeeDetails = {
+    empID : "12",
+    empName:"sita",
+    empCompany :"Amazon",
+    empSalary : "50,000",
+    empAddress :{
+        empCity:"Hyderabad",
+        empArea:"kukatpally",
+    }  
+ };
