7731643c0f877b7168b95f1721a845f236cfa966
Akshitha-eng <thathireddyakshithareddy@gmail.com>
2025-01-30 13:03:59 +0530

    demo10

---
+++ script1.js
@@ -0,0 +1,7 @@
+const collegeName="mru"
+const marks=100;
+function Tasking(){
+    console.log("I am learning code at "+collegeName);
+    document.getElementById("hello").innerText="I am learning code at "+collegeName
+}
+export { collegeName, Tasking };
